9e6ca074a2bb8c63f1de8f7466d8a9cf67c61805
Julius Olson <julius.olsons@gmail.com>
2020-10-10 17:30:20 +0200

    ADD theme and color conversion util

---
+++ lib/theme/style.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:rena/utils/colors.dart';
+
+ThemeData appTheme() {
+  return ThemeData(
+    primarySwatch: createMaterialColor(Color(0xFFF000539)),
+    accentColor: Colors.orange,
+    hintColor: Colors.white,
+    buttonColor: Colors.white,
+    scaffoldBackgroundColor: createMaterialColor(Color(0xFFF000539)),
+    textTheme: TextTheme(
+      bodyText1: TextStyle(
+        fontSize: 30,
+        color: Colors.white,
+        fontFamily: "Montserrat",
+      )
+    )     
+  );
+}
