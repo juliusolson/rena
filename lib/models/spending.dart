ae3c75f77a1e2fb102d693bc8881e0d18ab09103
Julius Olson <julius.olsons@gmail.com>
2020-10-10 23:24:32 +0200

    add simple models for charts

---
+++ lib/models/spending.dart
@@ -0,0 +1,18 @@
+
+class WeeklySpending {
+  final String week;
+  final num spendings;
+  WeeklySpending(this.week, this.spendings);
+}
+
+
+/// Sample linear data type.
+class SpendingCategory {
+  final int amount;
+  final int n;
+  final String name;
+  final String color;
+
+  SpendingCategory(this.amount, this.n, this.name, this.color);
+}
+
